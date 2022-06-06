8f4c85611f1b3bfefc7343ef4fd5c315868dbf84
Irena Georgieva <irenayordgeorgieva@gmail.com>
2022-06-06 11:35:31 +0300

    Fixed default proptype

--- src/components/KeypadButton.js
+++ src/components/KeypadButton.js
@@ -21,10 +21,14 @@ function KeypadButton({ text, value: buttonValue, onClick, disabled, color = 'li
 
 KeypadButton.propTypes = {
   color: PropTypes.string.isRequired,
-  disabled: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired
+}
+
+KeypadButton.defaultProps = {
+  disabled: false
 }
 
 export default KeypadButton
